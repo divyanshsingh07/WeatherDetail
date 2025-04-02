462e7197140f83adc62bfb1bcc87d0e23e4da820
Divyansh Singh <divyanshsingh@Divyanshs-MacBook-Air.local>
2025-04-03 00:45:02 +0530

    home link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-2 shadow-sm">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
-        <Link to={"/"} className="hover:opacity-80 transition-opacity">
+        <Link to="https://cloudi-taupe.vercel.app/" className="hover:opacity-80 transition-opacity">
           <h1 className="text-2xl font-bold text-foreground">CloudEye</h1>
         </Link>
 
